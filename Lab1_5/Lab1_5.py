689fafe364d7f7ec819d6c422a9ed6bb7367336a
Serhii Denysenko <serhii.denysenko.consultant@msccruises.com>
2023-02-01 07:52:47 +0200

    lab done

--- Lab1_5/Lab1_5.py
+++ Lab1_5/Lab1_5.py
@@ -1,1 +1,31 @@
+class Delta:
+    def __init__(self):
+        self.UniqueId = id(self)
+    
+    def getUniqueId(self):
+        return self.UniqueId
+
+
+class Delta1:
+    idCounter = 0
+    def __init__(self):
+        self.UniqueId = Delta1.idCounter
+        Delta1.idCounter = Delta1.idCounter + 1
+
+    def getUniqueId(self):
+        return self.UniqueId
+
+
+arr = []
+
+for i in range(14):
+    x = Delta()
+    print(x.getUniqueId())
+    arr.append(x)
+
+for i in range(14):
+    x = Delta1()
+    print(x.getUniqueId())
+    arr.append(x)
+
 
